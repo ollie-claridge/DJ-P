43c74e2cb9deb2d989366c8988c2f6238924e7db
[Ollie Claridge] <[ollie.claridge@gmail.com]>
2023-05-07 18:16:09 +0100

    Choosing Java Method

--- JS/JokeData.js
+++ JS/JokeData.js
@@ -3,13 +3,3 @@ import { apiKey } from "./apiKey";
 async function getJoke(getJoke){
     try{
 
-    const res = await fetch(
-       `https://dad-jokes.p.rapidapi.com/random/joke`
-    )
-        const json = await res.json();}
-    catch{
-console.log("error");
-    }
-}
-
-export {getJoke};
